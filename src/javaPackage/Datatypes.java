c79a55493986e2c848e1bfcbfe6a1a0061dcd6cf
Eshwari <mujgulee@gmail.com>
2025-07-02 20:29:41 +0530

    JavaProjectFirstCommit

---
+++ src/javaPackage/Datatypes.java
@@ -0,0 +1,40 @@
+package javaPackage;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		//Primitive Dataypes - int
+		int a=10, b=20, c;
+		c=a+b;
+		System.out.println("Addition is = "+c);
+		
+		//Primitive Datatype - double
+		double d=11.00, e=22.00, f;
+		f=d+e;
+		System.out.println("Addition is = "+f);
+		
+		//Primitive Datatype - char
+		char char1='E';
+		System.out.println("Character value = "+char1);
+		
+		//Primitive Datatype - boolean
+		boolean b1=true, b2=false;
+		System.out.println(b1);
+		System.out.println(b2);
+		
+		//Non-Primitive Dataypes - array
+		int arr1[]=new int[10];
+		arr1[0]=1;
+		arr1[1]=2;
+		arr1[2]=3;
+		System.out.println(arr1[2]);
+		
+		//Non-Primitive Dataypes - String
+		String str="Eshwari";
+		System.out.println(str);
+		
+
+		
+	}
+
+}
